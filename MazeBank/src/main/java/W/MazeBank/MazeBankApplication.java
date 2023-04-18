fa29c3c822fdd2abd0e79e8193eb3ecf017769eb
SebastiaanvanVliet <690592@student.inholland.nl>
2023-04-18 14:55:05 +0200

    change init

--- MazeBank/src/main/java/W/MazeBank/MazeBankApplication.java
+++ MazeBank/src/main/java/W/MazeBank/MazeBankApplication.java
@@ -1,4 +1,4 @@
-package W.Mazebank;
+package W.MazeBank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- MazeBank/src/test/java/W/MazeBank/MazeBankApplicationTests.java
+++ MazeBank/src/test/java/W/MazeBank/MazeBankApplicationTests.java
@@ -1,4 +1,4 @@
-package W.Mazebank;
+package W.MazeBank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
