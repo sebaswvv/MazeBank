458474b2d6a9c8281eebf86ca4f79d7de16d567c
SebastiaanvanVliet <690592@student.inholland.nl>
2023-04-18 14:44:06 +0200

    Init fam

---
+++ Maze-bank/src/main/java/W/Mazebank/MazeBankApplication.java
@@ -0,0 +1,13 @@
+package W.Mazebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazeBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MazeBankApplication.class, args);
+	}
+
+}
---
+++ Maze-bank/src/test/java/W/Mazebank/MazeBankApplicationTests.java
@@ -0,0 +1,13 @@
+package W.Mazebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazeBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
