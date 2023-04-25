ec35950983df32927e9df33ff6029af0d8f13aac
moetwil <luc.moetwil@gmail.com>
2023-04-25 20:02:52 +0200

    advice aangepast

--- MazeBank/src/main/java/w/mazebank/advices/ApplicationExceptionHandler.java
+++ MazeBank/src/main/java/w/mazebank/advices/ApplicationExceptionHandler.java
@@ -1,23 +1,26 @@
 package w.mazebank.advices;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import w.mazebank.exceptions.UserNotFoundException;
-
-import java.util.HashMap;
-import java.util.Map;
+import w.mazebank.models.responses.ResponseHandler;
 
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
+    // handle general exception
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception e) {
+        return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
+    }
+
     // handle User Not Found Exception
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException.class)
-    public Map<String, String> handleUserNotFoundException(UserNotFoundException e) {
-        // create new "json" for error message
-        Map<String, String> errors = new HashMap<>();
-        errors.put("message", e.getMessage());
-        return errors;
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException e) {
+        return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.MULTI_STATUS, null);
     }
 }
