e7166da030260cb8759ac9e851f2cf57eea7243f
moetwil <luc.moetwil@gmail.com>
2023-04-25 19:52:16 +0200

    GetAllUsers + Custom ResponseEntity body

---
+++ MazeBank/src/main/java/w/mazebank/models/responses/UserResponse.java
@@ -0,0 +1,16 @@
+package w.mazebank.models.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
--- MazeBank/src/main/java/w/mazebank/services/UserServiceJpa.java
+++ MazeBank/src/main/java/w/mazebank/services/UserServiceJpa.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import w.mazebank.exceptions.UserNotFoundException;
 import w.mazebank.models.User;
+import w.mazebank.models.responses.UserResponse;
 import w.mazebank.repositories.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserServiceJpa {
@@ -18,6 +22,22 @@ public class UserServiceJpa {
 
         // return the user
         return userRepository.findById(id).orElse(null);
+    }
+
+    public List<UserResponse> getAllUsers() {
+        List<User> users = userRepository.findAll();
+
+        // parse users to user responses
+        List<UserResponse> userResponses = new ArrayList<>();
+        for (User user : users) {
+            UserResponse userResponse = UserResponse.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .build();
+            userResponses.add(userResponse);
+        }
+        return userResponses;
     }
 
     public void addUser(User user) {
