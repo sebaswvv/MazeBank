0d2b9edb120245000e81b950eb3af3e214592188
SebastiaanvanVliet <690592@student.inholland.nl>
2023-05-11 16:04:45 +0200

    delete user implemented

---
+++ MazeBank/src/main/java/w/mazebank/models/responses/UserDeletedResponse.java
@@ -0,0 +1,15 @@
+package w.mazebank.models.responses;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDeletedResponse {
+    private String message;
+}
