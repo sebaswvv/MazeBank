4d494dbf357a64b024cc6e821091c4f36607c791
moetwil <luc.moetwil@gmail.com>
2023-05-26 09:40:14 +0200

    loginRequest DTO - validation toegevoegd & unit tests

--- MazeBank/src/main/java/w/mazebank/models/requests/LoginRequest.java
+++ MazeBank/src/main/java/w/mazebank/models/requests/LoginRequest.java
@@ -1,5 +1,8 @@
 package w.mazebank.models.requests;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +13,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequest {
-    // TODO: add validation
+
+    @NotNull(message = "Email is mandatory")
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
     private String email;
+
+    @NotNull(message = "Password is mandatory")
+    @NotBlank(message = "Password is mandatory")
     private String password;
 }
