042a9414ac81c744219cc6cd4980714811fead30
moetwil <luc.moetwil@gmail.com>
2023-05-26 10:17:47 +0200

    registerRequest DTO - model validation & unit tests

--- MazeBank/src/main/java/w/mazebank/models/requests/RegisterRequest.java
+++ MazeBank/src/main/java/w/mazebank/models/requests/RegisterRequest.java
@@ -1,9 +1,6 @@
 package w.mazebank.models.requests;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,22 +14,26 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "Email is mandatory")
+    @NotNull(message = "Email is mandatory")
     @Email(message = "Email should be valid")
     private String email;
 
+    @NotNull(message = "BSN is mandatory")
     private int bsn;
 
+    @NotNull(message = "First name is mandatory")
     @NotBlank(message = "First name is mandatory")
     private String firstName;
 
+    @NotNull(message = "Last name is mandatory")
     @NotBlank(message = "Last name is mandatory")
     private String lastName;
 
-    @NotBlank(message = "Password is mandatory")
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,}$", message = "Password should be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character")
     private String password;
 
-    @NotBlank(message = "Phone number is mandatory")
+    @NotNull(message = "Phone number is mandatory")
     @Pattern(regexp = "^[0-9]{10}$", message = "Phone number should be 10 digits")
     private String phoneNumber;
 
