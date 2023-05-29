6cc2bfbe7105407043fa735ea55aee37936bf7a0
SebastiaanvanVliet <690592@student.inholland.nl>
2023-05-29 14:07:54 +0200

    Account Patch Request

---
+++ MazeBank/src/test/java/w/mazebank/models/requests/AccountPatchRequestTest.java
@@ -0,0 +1,29 @@
+package w.mazebank.models.requests;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AccountPatchRequestTest {
+
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void testAbsoluteLimit() {
+        AccountPatchRequest accountPatchRequest = new AccountPatchRequest();
+        accountPatchRequest.setAbsoluteLimit(-1.0);
+        var violations = validator.validate(accountPatchRequest);
+        assertEquals(1, violations.size());
+        assertEquals("Absolute limit must be greater than 0", violations.iterator().next().getMessage());
+    }
+}
