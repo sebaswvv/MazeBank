b5d135e74b2b0f84cdf74c0e49849f79e7fb8ab7
SebastiaanvanVliet <690592@student.inholland.nl>
2023-05-29 16:05:43 +0200

    make test methods

---
+++ MazeBank/src/test/java/w/mazebank/services/TransactionServiceJpaTest.java
@@ -0,0 +1,33 @@
+package w.mazebank.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionServiceJpaTest {
+
+    @Test
+    // happy flow
+    void postTransaction() {
+    }
+
+    @Test
+    void checkIfSenderIsValid() {
+    }
+
+    @Test
+    void checkIfReceiverIsValid() {
+    }
+
+    @Test
+    void insufficientFunds() {
+    }
+
+    @Test
+    void checkIfAccountIsOwnedByUser() {
+    }
+
+    @Test
+    void checkIfUserIsAuthorized() {
+    }
+}
