2a24b1c1b8d0570050f915165cc3f751ffc5b9ac
moetwil <luc.moetwil@gmail.com>
2023-05-30 11:03:40 +0200

    pages aangemaakt

--- frontend/src/views/AtmView.vue
+++ frontend/src/views/AtmView.vue
@@ -1,5 +1,6 @@
 <template>
-    <h1>Home</h1>
+    <h1>ATM
+    </h1>
 </template>
 
 <script setup lang="ts">
--- frontend/src/views/CustomerDashboardView.vue
+++ frontend/src/views/CustomerDashboardView.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Home</h1>
+    <h1>Customer dashboard</h1>
 </template>
 
 <script setup lang="ts">
