1ba74d7ef5b85fab93351bbeda7815aa95fa3fb2
moetwil <luc.moetwil@gmail.com>
2023-05-30 21:07:05 +0200

    werkt

--- MazeBank/src/test/java/w/mazebank/cucumberglue/UsersStepDefinitions.java
+++ MazeBank/src/test/java/w/mazebank/cucumberglue/UsersStepDefinitions.java
@@ -6,9 +6,14 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 import w.mazebank.enums.RoleType;
 import w.mazebank.models.User;
+import w.mazebank.models.requests.RegisterRequest;
 import w.mazebank.services.JwtService;
 
 import java.time.LocalDate;
@@ -37,19 +42,25 @@ public class UsersStepDefinitions extends BaseStepDefinitions{
         // User user1 = new User(2, "user1@example.com", 123456789, "John", "Doe", "$2a$10$CHn7sYgipDQqx4yvV.X59.c07V9sTDiGmKfnlEBz48yznkDm7o6a.", "1234567890", RoleType.EMPLOYEE, LocalDate.now().minusYears(25), LocalDateTime.now(), 1000, 100, false, null);
         User user3 = new User(4, "user3@example.com", 456123789, "Jim", "John", "$2a$10$CHn7sYgipDQqx4yvV.X59.c07V9sTDiGmKfnlEBz48yznkDm7o6a.", "0987654321", RoleType.EMPLOYEE, LocalDate.now().minusYears(30), LocalDateTime.now(), 5000, 200, false, null);
 
-
         // create good token
         jwtService = new JwtService();
-        // token = jwtService.generateToken(user3);
-        System.out.println(token);
-
+        token = jwtService.generateToken(user3);
 
         httpHeaders.clear();
         httpHeaders.add("Authorization", "Bearer " + token);
         httpHeaders.add("Content-Type", "application/json");
-        // csrfToken
-        // httpHeaders.add("X-XSRF-TOKEN", "token");
-        lastResponse = restTemplate.getForEntity("http://localhost:" + port + "/users", String.class);
+
+        // Create the HTTP entity with the request body and headers
+        HttpEntity<Object> requestEntity = new HttpEntity<>(null, httpHeaders);
+
+        // Send the request
+        RestTemplate restTemplate = new RestTemplate();
+        lastResponse = restTemplate.exchange(
+            "http://localhost:" + port + "/users",
+            HttpMethod.GET, // Adjust the HTTP method if necessary
+            requestEntity,
+            String.class
+        );
     }
 
     @Then("the result is a list of user of size {int}")
