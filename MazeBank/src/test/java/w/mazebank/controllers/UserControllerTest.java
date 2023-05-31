bf09ed3edd640ca1be4e2ddbc49381f7389533d3
SebastiaanvanVliet <690592@student.inholland.nl>
2023-05-31 12:13:24 +0200

    block and unblock works

--- MazeBank/src/test/java/w/mazebank/controllers/UserControllerTest.java
+++ MazeBank/src/test/java/w/mazebank/controllers/UserControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import w.mazebank.enums.RoleType;
+import w.mazebank.exceptions.AccountNotFoundException;
 import w.mazebank.exceptions.UnauthorizedUserAccessException;
 import w.mazebank.exceptions.UserNotFoundException;
 import w.mazebank.models.User;
@@ -14,9 +15,12 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,6 +33,7 @@ class UserControllerTest extends BaseControllerTest {
 
         mockMvc.perform(get("/users/1")
                 .header("Authorization", "Bearer " + customerToken)
+                .with(csrf())
                 .with(user(authCustomer))
             ).andDo(print())
             .andExpect(status().isOk())
@@ -48,22 +53,11 @@ class UserControllerTest extends BaseControllerTest {
         // Expect 404, because the user does not exist. And expect a message: "User not found"
         mockMvc.perform(get("/users/1")
                 .header("Authorization", "Bearer " + customerToken)
+                .with(csrf())
                 .with(user(authCustomer))
             ).andDo(print())
             .andExpect(status().isNotFound())
             .andExpect(jsonPath("$.message").value("User not found with id: 1"));
-    }
-
-    @Test
-    void getUserByIdShouldReturnStatus401IfUnauthorized() throws Exception {
-        when(jwtService.extractEmail(Mockito.anyString())).thenReturn("user1@example.com");
-        when(jwtService.isTokenValid(Mockito.anyString(), Mockito.any())).thenThrow(new SignatureException("Unauthorized"));
-
-        mockMvc.perform(get("/users/1")
-                .header("Authorization", "Bearer invalid-token")
-            ).andDo(print())
-            .andExpect(status().isUnauthorized())
-            .andExpect(jsonPath("$.message").value("Unauthorized"));
     }
 
     @Test
@@ -73,6 +67,7 @@ class UserControllerTest extends BaseControllerTest {
         // Expect 403, because the user is not an employee or admin. And expect a message: "Forbidden"
         mockMvc.perform(get("/users/3")
                 .header("Authorization", "Bearer " + customerToken)
+                .with(csrf())
                 .with(user(authCustomer))
             ).andDo(print())
             .andExpect(status().isForbidden())
@@ -85,6 +80,7 @@ class UserControllerTest extends BaseControllerTest {
 
         mockMvc.perform(get("/users/1")
                 .header("Authorization", "Bearer " + employeeToken)
+                .with(csrf())
                 .with(user(authEmployee))
             ).andDo(print())
             .andExpect(status().isOk())
@@ -106,6 +102,7 @@ class UserControllerTest extends BaseControllerTest {
 
         mockMvc.perform(get("/users")
                 .header("Authorization", "Bearer " + employeeToken)
+                .with(csrf())
                 .with(user(authEmployee))
             ).andDo(print())
             .andExpect(status().isOk())
@@ -124,6 +121,7 @@ class UserControllerTest extends BaseControllerTest {
 
         mockMvc.perform(get("/users?limit=1")
                 .header("Authorization", "Bearer " + employeeToken)
+                .with(csrf())
                 .with(user(authEmployee))
             ).andDo(print())
             .andExpect(status().isOk())
@@ -132,25 +130,82 @@ class UserControllerTest extends BaseControllerTest {
             .andExpect(jsonPath("$[0].lastName").value("Doe"));
     }
 
-    // 401, wrong JWT
-    @Test
-    void getAllUsersShouldReturnStatus401IfUnauthorized() throws Exception {
-        when(jwtService.extractEmail(Mockito.anyString())).thenReturn("user1@example.com");
-        when(jwtService.isTokenValid(Mockito.anyString(), Mockito.any())).thenThrow(new SignatureException("Unauthorized"));
-
-        mockMvc.perform(get("/users")
-                .header("Authorization", "Bearer invalid-token")
-            ).andDo(print())
-            .andExpect(status().isUnauthorized())
-            .andExpect(jsonPath("$.message").value("Unauthorized"));
-    }
-
     @Test
     void getAllUsersShouldReturnStatus403IfForbidden() throws Exception {
         when(userServiceJpa.getAllUsers(0, 10, "asc", null)).thenReturn(userResponses);
 
         mockMvc.perform(get("/users")
                 .header("Authorization", "Bearer " + employeeToken)
+                .with(csrf())
+                .with(user(authCustomer))
+            ).andDo(print())
+            .andExpect(status().isForbidden())
+            .andExpect(jsonPath("$.message").value("Access Denied"));
+    }
+
+    // block & unblock user
+    @Test
+    void disableUserShouldReturnStatus200IfEmployee() throws Exception {
+        mockMvc.perform(put("/users/1/disable")
+                .header("Authorization", "Bearer " + employeeToken)
+                .with(csrf())
+                .with(user(authEmployee))
+            ).andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.locked").value(true));
+    }
+
+    @Test
+    void disableUserShouldReturn404IfUserNotFound() throws Exception {
+        doThrow(new UserNotFoundException("User not found with id: " + 1))
+            .when(userServiceJpa).blockUser(1L);
+
+        mockMvc.perform(put("/users/1/disable")
+                .header("Authorization", "Bearer " + employeeToken)
+                .with(user(authEmployee))
+            ).andDo(print())
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.message").value("User not found with id: 1"));
+    }
+
+    @Test
+    void disableUserShouldReturn403WhenUserPerformingIsNotACustomer() throws Exception {
+        mockMvc.perform(put("/users/1/disable")
+                .header("Authorization", "Bearer " + customerToken)
+                .with(user(authCustomer))
+            ).andDo(print())
+            .andExpect(status().isForbidden())
+            .andExpect(jsonPath("$.message").value("Access Denied"));
+    }
+
+    @Test
+    void enableUserShouldReturnStatus200IfEmployee() throws Exception {
+        mockMvc.perform(put("/users/1/enable")
+                .header("Authorization", "Bearer " + employeeToken)
+                .with(csrf())
+                .with(user(authEmployee))
+            ).andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.locked").value(false));
+    }
+
+    @Test
+    void enableUserShouldReturn404IfUserNotFound() throws Exception {
+        doThrow(new UserNotFoundException("User not found with id: " + 1))
+            .when(userServiceJpa).unblockUser(1L);
+
+        mockMvc.perform(put("/users/1/enable")
+                .header("Authorization", "Bearer " + employeeToken)
+                .with(user(authEmployee))
+            ).andDo(print())
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.message").value("User not found with id: 1"));
+    }
+
+    @Test
+    void enableUserShouldReturn403WhenUserPerformingIsNotACustomer() throws Exception {
+        mockMvc.perform(put("/users/1/enable")
+                .header("Authorization", "Bearer " + customerToken)
                 .with(user(authCustomer))
             ).andDo(print())
             .andExpect(status().isForbidden())
