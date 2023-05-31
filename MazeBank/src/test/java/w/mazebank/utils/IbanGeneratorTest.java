5fea27b4ac46cadccc18e7c7a17ee00a06cb3378
SebastiaanvanVliet <690592@student.inholland.nl>
2023-05-31 13:39:43 +0200

    Update IbanGeneratorTest.java

--- MazeBank/src/test/java/w/mazebank/utils/IbanGeneratorTest.java
+++ MazeBank/src/test/java/w/mazebank/utils/IbanGeneratorTest.java
@@ -44,7 +44,7 @@ class IbanGeneratorTest {
     @Test
     void accountNumberShouldBeGreaterThan1() {
         assertTrue(Integer.parseInt(iban.substring(8, 18)) > 1);
-        
+
         // DIT HIERONDER WERKTE NIET TIEMEN
         // System.out.println(iban.substring(8, 18));
         // assertEquals(1, iban.substring(8, 18).compareTo("0000000001"));
