4afe6fdf3e29009ad77865c50ae509a34cf9820d
moetwil <luc.moetwil@gmail.com>
2023-06-02 12:35:10 +0200

    register

--- frontend/src/stores/AuthenticationStore.ts
+++ frontend/src/stores/AuthenticationStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia';
 import axios from '../utils/axios';
-import Login from '../interfaces/Login';
+import Login from '../interfaces/requests/Login';
+import Register from '../interfaces/requests/Register';
 import AuthState from '../interfaces/AuthState';
 
 // STORE
@@ -44,19 +45,19 @@ export const useAuthenticationStore = defineStore({
 
       this.router.push('/login');
     },
-    // async register(newUser: User) {
-    //   try {
-    //     const response = await axios.post('/users/register', newUser);
-    //     if (response.status === 200) {
-    //       this.setUser(response.data.user, response.data.jwt);
-    //       axios.updateAuthorizationHeader(response.data.jwt);
-    //       // refresh page to update navbar
-    //       window.location.reload();
-    //     }
-    //   } catch (error: any) {
-    //     console.error(error);
-    //   }
-    // },
+    async register(registerRequest: Register) {
+      try {
+        const response = await axios.post('/auth/register', registerRequest);
+        if (response.status === 200) {
+          this.setUser(response.data.authenticationToken);
+          axios.updateAuthorizationHeader(response.data.jwt);
+          // refresh page to update navbar
+          window.location.reload();
+        }
+      } catch (error: any) {
+        console.error(error);
+      }
+    },
     // async updateUser(updateUser: User) {
     //   this.user = updateUser;
     //   try {
@@ -72,30 +73,19 @@ export const useAuthenticationStore = defineStore({
     setUser(token: string) {
       // set token in local storage
       localStorage.setItem('token', token);
-
-      // get userId from token
-      const decodeJwt = (token: string) => {
-        const base64Url = token.split('.')[1];
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const decodedData = atob(base64);
-        const decodedToken = JSON.parse(decodedData);
-        return decodedToken;
-      };
-
-      const decodedToken = decodeJwt(token);
+      const decodedUserId = this.decodeJwtReturnsUserId(token);
 
       // set userId in local storage and store
-      localStorage.setItem('userId', decodedToken.userId);
-      this.userId = decodedToken.userId;
+      localStorage.setItem('userId', decodedUserId);
+      this.userId = decodedUserId;
       this.isLoggedIn = true;
     },
-    // async checkAuth() {
-    //   const userId = localStorage.getItem('userId');
-    //   if (userId && this.userId === null) {
-    //     const response = await axios.get('/users/' + userId);
-    //     // console.log(response.data);
-    //     // this.setUser(response.data, localStorage.getItem('token') || '');
-    //   }
-    // },
+    decodeJwtReturnsUserId(token: string) {
+      const base64Url = token.split('.')[1];
+      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      const decodedData = atob(base64);
+      const decodedToken = JSON.parse(decodedData);
+      return decodedToken.userId;
+    },
   },
 });
