050d6ee7b2ef283573bcf3d6470723c663bedfc1
SebastiaanvanVliet <690592@student.inholland.nl>
2023-06-05 14:29:30 +0200

    fixed

--- frontend/src/interfaces/requests/UserPatchRequest.ts
+++ frontend/src/interfaces/requests/UserPatchRequest.ts
@@ -3,8 +3,6 @@ interface UserPatchRequest {
   firstName?: string;
   lastName?: string;
   phoneNumber?: string;
-  dayLimit?: number;
-  transactionLimit?: number;
 }
 
 export default UserPatchRequest;
